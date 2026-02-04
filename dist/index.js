47478a5d172a76e35a317ff1b99fb38a342622b0
Anshika Semwal <semwal.anshi.10@gmail.com>
2026-02-04 18:00:54 +0530

    Day 2 : Setup Express server with TypeScript

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+//Midleware to parse JSON
+app.use(express_1.default.json());
+// Test route
+app.get("/", (req, res) => {
+    res.send("BookBarberNow backend is running");
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("bookBarberNow backend started")
+import express from "express";
+
+const app = express();
+
+//Midleware to parse JSON
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+    res.send("BookBarberNow backend is running");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+})
