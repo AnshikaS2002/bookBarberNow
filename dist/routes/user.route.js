5b670d7bae9e923344cb81de584bc4247eaa8942
Anshika Semwal <semwal.anshi.10@gmail.com>
2026-02-16 20:13:35 +0530

    Day 14: Add proper Typescript typing for req.user

--- dist/routes/user.route.js
+++ dist/routes/user.route.js
@@ -2,7 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const user_controller_1 = require("../controllers/user.controller");
+const auth_middleware_1 = require("../middlewares/auth.middleware");
 const router = (0, express_1.Router)();
 router.post("/signup", user_controller_1.signupUser);
 router.post("/login", user_controller_1.loginUser);
+router.get("/profile", auth_middleware_1.authMiddleware, (req, res) => {
+    return res.status(200).json({
+        message: "Access granted to protected route",
+        userId: req.user?.userId,
+    });
+});
 exports.default = router;
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import {signupUser, loginUser} from "../controllers/user.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
+
+router.get("/profile", authMiddleware, (req, res) =>{
+    return res.status(200).json({
+        message : "Access granted to protected route",
+        userId : req.user?.userId,
+    });
+})
 
 export default router;
